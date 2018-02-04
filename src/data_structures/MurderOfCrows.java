2170dc1a57ca631f9859807b6bfec31e54ae275f
dalidog <league@WTS19.attlocal.net>
2018-02-04 11:30:34 -0800

    er654656n    fhb

--- src/data_structures/MurderOfCrows.java
+++ src/data_structures/MurderOfCrows.java
@@ -8,83 +8,95 @@ package data_structures;
  *    Platform=Eclipse
  *    Type=Recipe
  *    Objectives=n/a
-*/
+ */
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MurderOfCrows {
 
-    ArrayList<Crow> theMurder = new ArrayList<Crow>();
+	ArrayList<Crow> theMurder = new ArrayList<Crow>();
 
-    public static void main(String[] args) {
-        MurderOfCrows murderOfCrows = new MurderOfCrows();
-        murderOfCrows.initializeCrows();
-        murderOfCrows.findTheDiamond();
-    }
+	public static void main(String[] args) {
+		MurderOfCrows murderOfCrows = new MurderOfCrows();
+		murderOfCrows.initializeCrows();
+		murderOfCrows.findTheDiamond();
+	}
 
-    private void findTheDiamond() {
-    	System.out.println("hi");
-        /*
-         * 1. One of the Crows has eaten the diamond. You need to search through the stomach of each Crow, 
-         * then print the name of the guilty Crow.
-         */
-        
-        /* 2. How many innocent crows had to die before the diamond was found? */
-    	for (int i = 0; i < theMurder.size(); i++) {
+	private void findTheDiamond() {
+		System.out.println("hi");
+		/*
+		 * 1. One of the Crows has eaten the diamond. You need to search through the stomach of each Crow, 
+		 * then print the name of the guilty Crow.
+		 */
+		boolean foundDiamond =new Boolean(false);
+		/* 2. How many innocent crows had to die before the diamond was found? */
+		for (int i = 0; i < theMurder.size(); i++) {
 			ArrayList<String> x =theMurder.get(i).getStomachContents();
-			if(x.get(i) == "diamond") {
-		System.out.println(theMurder.get(i));
+			for (int j = 0; j < x.size(); j++) {
+				if(x.get(j) == "diamond") {
+					System.out.println(theMurder.get(i).getName());
+				
+					foundDiamond = true;
+				}
+
 			}
+			if(foundDiamond == false) {
+				System.out.println(theMurder.get(i).getName() + " did not have the diamond");
+			}
+
+
+
 		}
-        
-    }
+	}
 
-    private void initializeCrows() {
-        theMurder.add(new Crow("Rok"));
-        theMurder.add(new Crow("Merle"));
-        theMurder.add(new Crow("Poe"));
-        theMurder.add(new Crow("Grenwyn"));
-        theMurder.add(new Crow("Crawford"));
-        hideTheDiamond();
-    }
 
-    private void hideTheDiamond() {
-        int randomness = new Random().nextInt(theMurder.size());
-        theMurder.get(randomness).getStomachContents().add("diamond");
-    }
+
+	private void initializeCrows() {
+		theMurder.add(new Crow("Rok"));
+		theMurder.add(new Crow("Merle"));
+		theMurder.add(new Crow("Poe"));
+		theMurder.add(new Crow("Grenwyn"));
+		theMurder.add(new Crow("Crawford"));
+		hideTheDiamond();
+	}
+
+	private void hideTheDiamond() {
+		int randomness = new Random().nextInt(theMurder.size());
+		theMurder.get(randomness).getStomachContents().add("diamond");
+	}
 }
 
 class Crow {
-    private String name;
-    private ArrayList<String> stomachContents = new ArrayList<String>();
+	private String name;
+	private ArrayList<String> stomachContents = new ArrayList<String>();
 
-    public Crow(String name) {
-        this.name = name;
-        fillCrowsStomach();
-    }
+	public Crow(String name) {
+		this.name = name;
+		fillCrowsStomach();
+	}
 
-    private void fillCrowsStomach() {
-        for (int i = 0; i < 10; i++)
-            this.stomachContents.add(getRandomCrowFood());
-    }
+	private void fillCrowsStomach() {
+		for (int i = 0; i < 10; i++)
+			this.stomachContents.add(getRandomCrowFood());
+	}
 
-    private String getRandomCrowFood() {
-        int randomness = new Random().nextInt(4);
-        if (randomness == 0) return "carrion";
-        else if (randomness == 1)return "snail";
-        else if (randomness == 2)return "acorn";
-        else if (randomness == 3)return "spider";
-        else return "grub";
-    }
+	private String getRandomCrowFood() {
+		int randomness = new Random().nextInt(4);
+		if (randomness == 0) return "carrion";
+		else if (randomness == 1)return "snail";
+		else if (randomness == 2)return "acorn";
+		else if (randomness == 3)return "spider";
+		else return "grub";
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public ArrayList<String> getStomachContents() {
-        return stomachContents;
-    }
+	public ArrayList<String> getStomachContents() {
+		return stomachContents;
+	}
 
 }
 
--- src/data_structures/RemovingStuffFromArrayLists.java
+++ src/data_structures/RemovingStuffFromArrayLists.java
@@ -2,21 +2,26 @@ package data_structures;
 
 import java.util.ArrayList;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 /**
  * Copyright The League of Amazing Programmers 2013-2017 Level 3 Two ArrayList
  * Cleaning Exercises Duration=.00 Platform=Eclipse Type=Recipe Objectives=
  */
+class Stuff {
+	
 
+}
+class Worm extends Stuff {
+}
+class Dirt extends Stuff {
+}
 public class RemovingStuffFromArrayLists {
 
 	public static void main(String[] args) {
 
-		class Stuff {
-		}
-		class Worm extends Stuff {
-		}
-		class Dirt extends Stuff {
-		}
+		
+		
 		ArrayList<Stuff> stuffIFoundInTheYard = new ArrayList<Stuff>();
 		stuffIFoundInTheYard.add(new Worm());
 		stuffIFoundInTheYard.add(new Dirt());
@@ -25,10 +30,14 @@ public class RemovingStuffFromArrayLists {
 		stuffIFoundInTheYard.add(new Worm());
 		stuffIFoundInTheYard.add(new Dirt());
 
-		System.out.println(stuffIFoundInTheYard.size());
+		//System.out.println(stuffIFoundInTheYard.size());
 
 		/* TODO 1: Clean out the dirt but keep the delicious worms. */
-
+		for (int i = 0; i < stuffIFoundInTheYard.size(); i++) {
+			if(stuffIFoundInTheYard.get(i) instanceof Dirt) {
+				stuffIFoundInTheYard.remove(i);
+			}
+		}
 		System.out.println(stuffIFoundInTheYard.size()); // should be 2
 
 		ArrayList<Character> truth = new ArrayList<Character>();
